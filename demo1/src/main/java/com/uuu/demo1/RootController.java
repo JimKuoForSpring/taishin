621c16e3bc9763d73228ba6ba69ccd74ef547326
Jim <jim832011@gmail.com>
2022-04-16 16:31:26 +0800

    pass parameter to html

--- demo1/src/main/java/com/uuu/demo1/RootController.java
+++ demo1/src/main/java/com/uuu/demo1/RootController.java
@@ -1,12 +1,14 @@
 package com.uuu.demo1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RootController {
     @GetMapping("/")
-    public String home(){
+    public String home321(Model model) {
+        model.addAttribute("message","Hi, I am Jim");
         return "home";
     }
 }
