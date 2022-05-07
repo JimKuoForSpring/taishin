7339b7c36a8ce680966c726933118fe5a5cb5134
Jim <jim832011@gmail.com>
2022-05-07 14:08:09 +0800

    add controller

--- demo2/src/main/java/com/example/demo2/demo2/Demo2Application.java
+++ demo2/src/main/java/com/example/demo2/demo2/Demo2Application.java
@@ -2,12 +2,19 @@ package com.example.demo2.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Demo2Application {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo2Application.class, args);
+		org.springframework.context.ConfigurableApplicationContext run = SpringApplication.run(Demo2Application.class, args);
 	}
 
 }
